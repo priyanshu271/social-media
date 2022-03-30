7ae44c92750df3d8ea3002c3fef6bea848341c76
Priyanshu <priyanshurai271@gmail.com>
2022-03-30 21:44:38 +0530

    feat: changed directory structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
+import { RegisterComponent } from './home/register/register.component';
+import { LoginComponent } from './home/login/login.component';
 
 @NgModule({
   declarations: [
